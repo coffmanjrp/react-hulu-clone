e17058d0d70a382e2b134f34f6209d815f11c2e4
coffmanjrp <coffmanjrp@gmail.com>
2021-08-27 23:41:44 -0700

    HTML and styles

---
+++ src/img/index.js
@@ -0,0 +1,41 @@
+import logo from './logo.png';
+import logos from './logos.png';
+import liveSportsLogo1 from './live-sports-logo-1.png';
+import liveSportsLogo2 from './live-sports-logo-2.png';
+import liveSportsLogo3 from './live-sports-logo-3.svg';
+import liveSportsLogo4 from './live-sports-logo-4.png';
+import breakingNewsLogo1 from './breaking-news-logo-1.png';
+import breakingNewsLogo2 from './breaking-news-logo-2.svg';
+import breakingNewsLogo3 from './breaking-news-logo-3.png';
+import breakingNewsLogo4 from './breaking-news-logo-4.svg';
+import biggestEventsLogo1 from './biggest-events-logo-1.png';
+import biggestEventsLogo2 from './biggest-events-logo-2.png';
+import biggestEventsLogo3 from './biggest-events-logo-3.png';
+import biggestEventsLogo4 from './biggest-events-logo-4.png';
+import facebook from './facebook.svg';
+import twitter from './twitter.svg';
+import youtube from './youtube.svg';
+import instagram from './instagram.svg';
+import close from './close.svg';
+
+export {
+  logo,
+  logos,
+  liveSportsLogo1,
+  liveSportsLogo2,
+  liveSportsLogo3,
+  liveSportsLogo4,
+  breakingNewsLogo1,
+  breakingNewsLogo2,
+  breakingNewsLogo3,
+  breakingNewsLogo4,
+  biggestEventsLogo1,
+  biggestEventsLogo2,
+  biggestEventsLogo3,
+  biggestEventsLogo4,
+  facebook,
+  twitter,
+  youtube,
+  instagram,
+  close,
+};
